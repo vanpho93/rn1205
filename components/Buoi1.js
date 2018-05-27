a8b9a79430c1bf405df61ebcd4777dbceb4b563b
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-27 11:07:46 +0700

    Buoi1.js

--- components/Buoi1.js
+++ components/Buoi1.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default class Buoi1 extends Component {
     render() {
         return (
-            <View style={{ margin: 20 }}>
-                <Text>Buoi1 Component</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Buoi1 Component</Text>
+                <TouchableOpacity
+                    style={styles.buttonContainer} onPress={() => alert('Hello')}
+                >
+                    <Text style={styles.buttonText}>SHOW</Text>
+                </TouchableOpacity>
             </View>
         );
+    }
+}
+
+const styles = {
+    container: {
+        padding: 20,
+        backgroundColor: 'green',
+        flex: 1
+    },
+    title: {
+        fontSize: 30,
+        color: 'white'
+    },
+    buttonContainer: {
+        backgroundColor: '#6F5591',
+        padding: 10,
+        width: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        borderColor: '#A08FB8',
+        borderWidth: 2
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold'
     }
 }
