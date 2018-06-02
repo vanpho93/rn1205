03b3e566959c8e6282be49d8dc7304ee2731107a
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-02 13:51:55 +0700

    image

--- components/Buoi1.js
+++ components/Buoi1.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Word from './Word';
 import MyInput from './MyInput';
 
@@ -10,6 +10,11 @@ export default class Buoi1 extends Component {
                 <Text style={styles.title}>Buoi1 Component</Text>
                 <Word />
                 <MyInput />
+                <Image source={require('../images/a.png')} />
+                <Image
+                    source={{ uri: 'https://zmp3-photo.zadn.vn/banner/1/3/0/2/13022cd8a45de1c3e5bbce053f436254.jpg' }}
+                    style={{ width: 300, height: 100 }}
+                />
             </View>
         );
     }
