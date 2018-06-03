959fd4cc84bab28afc223a1d1d8606a184a456b3
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-03 11:43:01 +0700

    load more

--- components/Singers.js
+++ components/Singers.js
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, FlatList, Image, RefreshControl } from 'react-native';
+import axios from 'axios';
 
 export default class Singers extends Component {
     state = {
-        singers: [
-            {
-                "_id": "bb05f770-6dd4-4d87-a3a4-22ed5221ff2a",
-                "name": "Melyna",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/naitanamoreno/128.jpg",
-                "email": "Melyna.White@yahoo.com"
-            },
-            {
-                "_id": "08217ad1-930e-45cf-8baf-2a6454259459",
-                "name": "Olaf",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/the_winslet/128.jpg",
-                "email": "Olaf.Bernhard@gmail.com"
-            },
-            {
-                "_id": "1b2c8472-2490-411e-9257-3ac6cac386a2",
-                "name": "Nash",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/kurafire/128.jpg",
-                "email": "Nash_Altenwerth@gmail.com"
-            },
-            {
-                "_id": "a039b7c5-04da-4ddb-a168-a0056b485be1",
-                "name": "Jesus",
-                "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/artvavs/128.jpg",
-                "email": "Jesus24@yahoo.com"
-            }
-        ],
+        singers: [],
         loading: false
+    }
+
+    componentDidMount() {
+        this.getMoreSinger();
+    }
+
+    getMoreSinger = async () => {
+        this.setState({ loading: true });
+        const { data } = await axios.get('https://user1205.herokuapp.com/singer');
+        this.setState({
+            singers: data.singers.concat(this.state.singers),
+            loading: false
+        });
     }
 
     render() {
@@ -43,12 +32,7 @@ export default class Singers extends Component {
                     refreshControl={
                         <RefreshControl
                             refreshing={this.state.loading}
-                            onRefresh={() => {
-                                this.setState({ loading: true });
-                                setTimeout(() => {
-                                    this.setState({ loading: false });
-                                }, 3000);
-                            }}
+                            onRefresh={this.getMoreSinger}
                         />
                     }
                     renderItem={param => (
