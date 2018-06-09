779ac38cb8e13300dfbd1e65e403cc03eaeef673
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-09 15:03:52 +0700

    client

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import io from 'socket.io-client';
 import Button from './shared/Button';
 import Input from './shared/Input';
 
+const socket = io('https://realtime1205.herokuapp.com/');
+
 export default class Chat extends Component {
     state = { txtMessage: '', lastMessage: '' }
+
+    componentDidMount() {
+        socket.on('SERVER_SEND_MESSAGE', data => this.setState({ lastMessage: data }));
+    }
+
+    sendMessage = () => {
+        socket.emit('CLIENT_SEND_MESSAGE', this.state.txtMessage);
+        this.setState({ txtMessage: '' });
+    }
+
     render() {
         const { txtMessage, lastMessage } = this.state;
         return (
             <View style={styles.container}>
-                <Text>{ lastMessage? lastMessage : 'Waiting for messages' }</Text>
+                <Text style={{ color: '#565D61' }}>{ lastMessage? lastMessage : 'Waiting for messages' }</Text>
                 <Input
                     placeholder="Enter your message here"
                     value={txtMessage}
@@ -18,6 +31,7 @@ export default class Chat extends Component {
                 <Button
                     title="Send Message"
                     type="success"
+                    onPress={this.sendMessage}
                 />
             </View>        
         );
